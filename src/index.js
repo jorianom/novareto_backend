796e4b8d1a3a9413e498a42b053b02bc3ce3d765
jorianom <jorianom@unal.edu.co>
2022-06-02 11:50:44 -0500

    add enviroment variables and express

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json()); //body parser
+
+app.listen(PORT, () => {
+    console.log(`Listening http://localhost:${PORT}`);
+});
