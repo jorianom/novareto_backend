30a147545aa22be762e3ad73f4ce3d1597bbcf3e
jorianom <jorianom@unal.edu.co>
2022-06-03 00:20:26 -0500

    fix validation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const conectarDB = require("./services/database");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json()); //body parser
 
 conectarDB();
+app.use(cors({ origin: "*" }));
 app.use("/api/mailbox", require("./routes/mailbox"));
 
 app.listen(PORT, () => {
--- src/routes/mailbox.js
+++ src/routes/mailbox.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const mailboxController = require("../controller/mailboxController");
-const {check} = require("express-validator");
+const { check } = require("express-validator");
 const router = express.Router();
 
 router.get("/", mailboxController.getMailboxes);
-router.post("/add",[
-    check("name").not().isEmpty(),
-    check("email").isEmail(),
-    check("celular").isMobilePhone("pt-BR"),
-    check("type").not().isEmpty(),
-    check("comments").not().isEmpty(),
-], mailboxController.addMailbox);
+router.post(
+    "/add",
+    [
+        check("name").not().isEmpty(),
+        check("email").isEmail(),
+        check("type").not().isEmpty(),
+        check("comments").not().isEmpty(),
+    ],
+    mailboxController.addMailbox
+);
 
 module.exports = router;
